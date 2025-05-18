96845236a62b5242eed5bc63afc1837a4c6f0882
souldew <souldew2002@gmail.com>
2025-05-18 23:24:05 +0900

    間違えてpushしたContainerを一度から削除

--- src/containers/CountButtonContainer/CountButtonContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/Button";
-import { useState } from "react";
-
-export const CountButtonContainer = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount((prev) => prev + 1);
-
-  return <Button onClick={increment}>Count: {count}</Button>;
-};
